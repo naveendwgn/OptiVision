55aeba305ebf4828759b3e52d0fb847dbe81f39f
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-02-24 15:02:28 +0530

    mid commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg'
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ParticlesBg className='particles' type="cobweb" bg={true} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     );
