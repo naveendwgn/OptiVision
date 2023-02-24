fbf4662dc137a3d386ff48cc2b2bdbe05f66b1f8
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-02-24 17:38:42 +0530

    add a p tag

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -7,6 +7,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
             <p className='f3'>
                 {'OptiVision lets you detect faces in your pictures. Give it a try.'}
             </p>
+            <p className='f6'>
+                {'Add a link to an image(jpg, jpeg or png) and click Detect.'}
+            </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
                     <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange} />
